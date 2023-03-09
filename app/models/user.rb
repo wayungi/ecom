e8b2f20733576f6d23ec122d88f15897b2530238
wayungi <fwayungi@gmail.com>
2023-03-10 01:02:16 +0300

    UPDATE:users table with password field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :fname, :lname :tel, :address, :role, presence: true  
+  validates :fname, :lname, :tel, :address, :role, presence: true  
   validates :fname, :lname, length: {maximum: 30 }    
-  validates :tel length:{maximum: 12}                                           
-  validates :addres, :email, length: {maximum:50} 
-  vaidates  :password, length:{in: 8..20}                                    
+  validates :tel, length: {maximum: 12}                                           
+  validates :address, :email, length: {maximum:50} 
+  validates  :password, length: {in: 8..20}                                    
 end
