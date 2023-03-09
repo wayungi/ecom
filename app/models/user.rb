65f8474c2e434b61ca520d0a205d8faa0466fa29
wayungi <fwayungi@gmail.com>
2023-03-10 01:10:12 +0300

    UPDATE: tel validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :fname, :lname, :tel, :address, :role, presence: true  
+  validates :fname, :lname, :tel, :address, :role, :email, presence: true  
   validates :fname, :lname, length: {maximum: 30 }    
-  validates :tel, length: {maximum: 12}                                           
+  validates :tel, length: {in: 10..12}                                           
   validates :address, :email, length: {maximum:50} 
   validates  :password, length: {in: 8..20}                                    
 end
