5a6cec01abf104d51c59f56c6e989ce5cec0cf30
wayungi <fwayungi@gmail.com>
2023-03-10 00:23:54 +0300

    ADD: validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :fname, :lname :tel, :address, :role, presence: true  
+  validates :fname, :lname, length: {maximum: 30 }    
+  validates :tel length:{maximum: 12}                                           
+  validates :addres, :email, length: {maximum:50} 
+  vaidates  :password, length:{in: 8..20}                                    
 end
